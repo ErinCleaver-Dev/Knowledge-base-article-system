3af976c115c1d6eb40177d6ac5c18a6b1dd6033e
ecleaver <erincleaver@grcc.email.edu>
2021-10-22 18:21:05 -0400

    Added Services

--- server/services/articleService.js
+++ server/services/articleService.js
@@ -8,7 +8,24 @@ module.exports = {
         let article = new Article(data); 
 
         return await article.save().then(result => {
-            console.log('Added job to list');
+            console.log('Added article');
+        })
+    },
+    edit : async function (_id ,data) {
+        return await Article.findByIdAndUpdate(_id, {
+            last_revised_date: data.last_revised_date,
+            title: data.title,
+            category: data.category,
+            key_terms: data.key_terms,
+            video: data.video,
+            post_content: data.post_content,
+        }).then(result => {
+            console.log('Updated article')
+        })
+    },
+    delete: async function (_id) {
+        return await Article.findByIdAndDelete(_id).then(result => {
+            console.log('Article Deleted')
         })
     },
     getOneByid : async function (_id) {
@@ -16,6 +33,16 @@ module.exports = {
     },
     findPosts : async function (search) {
         return await Article.find({$or: [{title: search}, {keywords: search}, {post_content: search}]}).lean();
-    }
+    },
+    getByUserid : async function (user) {
+        return await Article.find({user_id: user})
+    },
+    updateLike :  async function (_id ,like) {
+        return await Article.findByIdAndUpdate(_id, {
+            like: like,
+        }).then(result => {
+            console.log('Updated like counter')
+        })
+    },
 }
 
