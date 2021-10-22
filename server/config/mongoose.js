d54992c5a39144a6cea0ade49a425b51e720f275
ecleaver <erincleaver@grcc.email.edu>
2021-10-22 06:35:25 -0400

    update git

---
+++ server/config/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const config = require('./index')
+
+function setupMongoose(app) {
+
+    mongoose.connect(config.DB_CONNECTION).then(() => {
+        console.log('Database is connected')
+    })
+
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error'))
+    db.on('open', console.log.bind(console, 'connection is opened'))
+}
+
+module.exports = setupMongoose
