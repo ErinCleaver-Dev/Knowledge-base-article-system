383f3519c3f60199a1d8c97271c675445ca222f3
ecleaver <erincleaver@grcc.email.edu>
2021-10-24 11:07:12 -0400

    Added logo
    Added logo

--- client/src/components/layout/navbar/Navbar.js
+++ client/src/components/layout/navbar/Navbar.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { styled } from '@mui/material/styles';
 import {Box, Button} from '@mui/material'
+import logo from '../../../images/logo.svg'
 
 const NavagationBar = styled(Box) ({
     display: 'flex',
     justifyContent: 'spaceBetween',
     backgroundColor: '#28666E',
-    height: '100px',
+    padding: '20px',
 })
 
 const Navbar = () => {
     return (
         <NavagationBar>
-            
+            <img className="logoImage" src={logo} />
         </NavagationBar>
     )
 }
