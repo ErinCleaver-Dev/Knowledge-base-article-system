8236c537dc45fd023ad224121498ee06e8bf3b37
ecleaver <erincleaver@grcc.email.edu>
2021-10-24 13:37:22 -0400

    Completed Nav bar - Erin Cleaver
    Added the hamburger functionality navbar

--- client/src/components/layout/navbar/Navbar.js
+++ client/src/components/layout/navbar/Navbar.js
@@ -1,28 +1,85 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { styled } from '@mui/material/styles';
 import {Box, Button} from '@mui/material'
 import logo from '../../../images/logo.svg'
 import {NavLink} from 'react-router-dom'
 import ProfileCard from './ProfileCard'
+import MenuIcon from '@mui/icons-material/Menu';
 
-const NavagationBar = styled(Box) ({
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: '#28666E',
-    padding: '20px 40px',
-})
+// added state for hammburder button.
 
-const StyledNavLink = styled(NavLink) ({
-    color: 'white',
-    textDecoration: 'None',
-    fontSize: '1.4em',
-    fontWeight: 'bold',
-    paddingLeft: '60px'
-})
+
+
+
+
+
 
 const Navbar = (props) => {
+    const [displayed, setDisplayed] = useState('none')
+
+    const clickedHamburger = () => {
+        if(displayed == 'none') {
+            setDisplayed('flex')
+        } else {
+            setDisplayed('none')
+        }
+    }
+
+    
+    const NavagationBar = styled(Box) ({
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        position: 'relative',
+        backgroundColor: '#28666E',
+        padding: '20px 40px',
+        ['@media (max-width:1024px)']: {
+            padding: '20px 20px',
+            justifyContent: 'none',
+            alignItems: 'none',
+
+        }
+    })
+
+
+    const HamburgerButton = styled(MenuIcon) ({
+        color: 'white',
+        display: 'none',
+        paddingLeft: '40px',
+        ['@media (max-width:1024px)']: {
+            display: 'block'
+        }
+    })
+
+    const StyledNavLink = styled(NavLink) ({
+        color: 'white',
+        textDecoration: 'None',
+        fontSize: '1.4em',
+        fontWeight: 'bold',
+        paddingLeft: '60px',
+        ['@media (max-width:1024px)']: {
+            paddingLeft: '0'
+        }
+
+    })
+
+    const Hamburger = styled('div') ({
+        display: 'flex',
+        alignItems: 'center',
+        ['@media (max-width:1024px)']: {
+            display: displayed,
+            flexDirection: 'column',
+            alignItems: 'center',
+            marginTop: '70px',
+            position: 'absolute',
+            right: 0,
+            width: '100%',
+            index: '1',
+            backgroundColor: '#28666E'
+        }
+    })
+
     return (
         <NavagationBar>
             <img className="logoImage" src={logo} />
@@ -34,15 +91,22 @@ const Navbar = (props) => {
                     ) : 
                     (null)
                 }
-                <StyledNavLink to="/" >Home</StyledNavLink>
-                {props.loggedIn ? (
-                    <StyledNavLink to="/logout" >Logout</StyledNavLink>
-                ) : (
-                    <>
-                    <StyledNavLink to="/login" >Login</StyledNavLink>
-                    <StyledNavLink to="/Singup" >Logout</StyledNavLink>
-                    </>
-                )}
+                <HamburgerButton onClick={clickedHamburger}/>
+
+                <Hamburger>
+                    <StyledNavLink to="/" >Home</StyledNavLink>
+                    {props.loggedIn ? (
+                        <>
+                        <StyledNavLink to="/logout" >Logout</StyledNavLink>
+                        </>
+                        
+                    ) : (
+                        <>
+                        <StyledNavLink to="/login" >Login</StyledNavLink>
+                        <StyledNavLink to="/Singup" >Logout</StyledNavLink>
+                        </>
+                    )}
+                </Hamburger>
             </div>
         </NavagationBar>
     )
--- client/src/components/layout/sidebar/Sidebar.js
+++ client/src/components/layout/sidebar/Sidebar.js
@@ -4,10 +4,16 @@ import {Box, Button} from '@mui/material'
 
 const SidebarNav =  styled(Box) ({
     display: 'flex',
+    flexDirection: 'column',
     justifyContent: 'spacebetween',
     backgroundColor: '#28666E',
-    height: '800px',
+
     width: '200px',
+    ['@media (max-width:1024px)']: {
+        width: '100%',
+        height: '0',
+        flexDirection: 'row',
+    }
 })
 
 const Sidebar = () => {
