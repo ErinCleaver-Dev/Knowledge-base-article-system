454b872665dd564404688c2cd1943884b00e31cd
ecleaver <erincleaver@grcc.email.edu>
2021-10-30 14:54:09 -0400

    Added the search box functionality to the front end.
    Added the search box functionality to the front end.

--- client/src/components/layout/search_box/SearchBox.js
+++ client/src/components/layout/search_box/SearchBox.js
@@ -1,7 +1,10 @@
 import {OutlinedInput, InputAdornment} from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search';
 import { styled } from '@mui/material/styles';
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import Cookies from 'universal-cookie';
+import { useHistory } from 'react-router-dom';
+
 
 
 const SearchBoxInput = styled(OutlinedInput) ({
@@ -14,10 +17,29 @@ const SearchBoxInput = styled(OutlinedInput) ({
 })
 
 const SearchBox = () => {
+    const cookie = new Cookies();
+
+    const [search, setSearch] = useState(cookie.get('search') || '')
+    const history = useHistory();
+    const handleKeyPress = (event) => {
+        console.log(event)
+        
+        if(event.key == 'Enter') {
+            console.log("Search term entered ", event.target.value)
+            let d = new Date();
+            d.setTime(d.getTime() + (60*60*1000));
+            cookie.set("search", event.target.value, {path: '/', expires: d})
+            setSearch(event.target.value)
+            history.push(`/search?q=${event.target.value}`)
+        }
+    }
+
     return (
         <>
             <SearchBoxInput
                 id="outlined-adornment-amount"
+                onKeyUp={handleKeyPress}
+                defaultValue={search}
                 startAdornment={<InputAdornment position="start"><SearchIcon/></InputAdornment>}
             />
         </>
