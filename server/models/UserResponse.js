f1d5f72e1ba6fcc6a5678dc8f4cdf9ba40a0865c
ecleaver <erincleaver@grcc.email.edu>
2021-11-05 07:37:03 -0400

    update

--- server/models/UserResponse.js
+++ server/models/UserResponse.js
@@ -4,8 +4,8 @@ const userResponseSchema = new mongoose.Schema({
     id: mongoose.Schema.Types.ObjectId,
     userResponse_type: { type: String, required: true }, //['Issue', 'Comments', reply]
     post_content: { type: String, require: true },
-    comment_id: { type: mongoose.Schema.Types.ObjectId, ref: 'UserResponse' },
     article_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Article', require: true },
+    responseTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true }
 })
 
