dce369ce02630b6a21fa39eaefa4da17132df94a
ecleaver <erincleaver@grcc.email.edu>
2021-11-06 07:16:13 -0400

    updated the response model

--- server/services/userResponseService.js
+++ server/services/userResponseService.js
@@ -13,8 +13,12 @@ module.exports = {
       console.log("Testing getComments service", article_id)
       if(article_id)  {
         const comments = await UserResponse.find({$and: [{article_id: article_id}, {userResponse_type: {$ne: "reply"}}]})
+        .populate('user_id', {firstName: 1, lastName: 1}).exec() 
 
         const replys = await UserResponse.find({article_id: article_id}).where('userResponse_type').equals('reply')
+        .populate('user_id', {firstName: 1, lastName: 1}).exec() 
+
+        console.log(replys)
 
         return {
           comments: comments,
