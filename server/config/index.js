d69c2ed267b8eb8cf5e0e3b6cc11075c406e90c6
ecleaver <erincleaver@grcc.email.edu>
2021-11-23 22:02:42 -0500

    moved env

--- server/config/index.js
+++ server/config/index.js
@@ -1,10 +1,11 @@
+
 const config = {
     development: {
         port: process.env.PORT || 5000
     },
     production: {},
 
-    DB_CONNECTION: process.env.MONGODB_URI /*|| 'mongodb://localhost/kbadb'*/,
+    DB_CONNECTION: process.env.MONGODB_URI || 'mongodb://localhost/kbadb',
 
     cookie: 'x-auth-token'
 }
