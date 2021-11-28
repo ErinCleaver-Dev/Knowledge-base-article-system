24ac3315d8937c6d034d3a46e71fc16ce2260613
Jimmy Lo <83715466+JimmyCHL@users.noreply.github.com>
2021-11-28 14:19:04 -0500

    revise api for newpost and add setTimeOut for Likes

--- client/src/components/pages/articles/likes/Likes.js
+++ client/src/components/pages/articles/likes/Likes.js
@@ -1,68 +1,62 @@
-import React, {useState, useContext, createRef, useEffect} from 'react'
-import { useHistory, useLocation } from 'react-router-dom';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import { styled } from '@mui/material/styles';
-import axios from 'axios';
-import Config from '../../../../config/index'
-import { UserContext } from '../../../../App';
-import { ArticleInfoContext } from '../ViewArticle'
+import React, { useState, useContext, createRef, useEffect } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { styled } from "@mui/material/styles";
+import axios from "axios";
+import Config from "../../../../config/index";
+import { UserContext } from "../../../../App";
+import { ArticleInfoContext } from "../ViewArticle";
 
+const Likes = ({ likes, article_id }) => {
+  const history = useHistory();
+  const location = useLocation();
 
+  const [articleInfo, setArticleInfo] = useContext(ArticleInfoContext);
 
-const Likes = ({likes, article_id}) => {
-    const history = useHistory();
-    const location = useLocation();  
+  const newLike = {
+    article_id: articleInfo.article_id,
+    user_id: articleInfo.user_id,
+  };
 
-    const [articleInfo, setArticleInfo] = useContext
-    (ArticleInfoContext)
-    
-    const newLike = {
-        article_id: articleInfo.article_id,
-        user_id: articleInfo.user_id,
-    }
+  const clickHandler = () => {
+    console.log("Clicked like");
+    axios
+      .post(`${Config.URL}api/likeArticle`, {
+        newLike,
+      })
+      .then((response) => {
+        let deleteLike = response.data;
+        if (response) {
+          axios.post(`${Config.URL}api/deleteLike`, {
+            deleteLike,
+          });
+        }
+      });
 
-    const clickHandler = () => {
-        console.log('Clicked like')
-        axios.post(`${Config.URL}api/likeArticle`, {
-            newLike
-        }).then(response => {
-            let deleteLike = response.data
-            if(response) {
-                
-                axios.post(`${Config.URL}api/deleteLike`, {
-                    deleteLike
-                })
-            }
+    setTimeout(() => {
+      window.location.reload();
+    }, 500);
+  };
 
-         
-        })
+  useEffect(() => {
+    axios.post(`${Config.URL}api/updateLikeCounter`, {
+      article_id: articleInfo.article_id,
+    });
+  }, []);
 
-        window.location.reload()
-    }
+  const LikesIcon = styled(FavoriteIcon)({
+    color: "#033F63",
+    fontSize: "1.8em",
+    paddingLeft: "10px",
+    pointerEvents: localStorage.getItem("isLoggedIn") ? "auto" : "none",
+    cursor: "pointer",
+  });
 
-    useEffect(() =>{
-        axios.post(`${Config.URL}api/updateLikeCounter`, {
-            article_id: articleInfo.article_id,
-        })
-        
-    }, [])
+  return (
+    <>
+      <LikesIcon onClick={clickHandler} /> Likes: {likes ? likes : 0}
+    </>
+  );
+};
 
-
-    const LikesIcon = styled(FavoriteIcon) ({
-        color: '#033F63',
-        fontSize: '1.8em',
-        paddingLeft: '10px',
-        pointerEvents: localStorage.getItem('isLoggedIn') ? ('auto') : ('none'),
-        cursor: 'pointer',
-    })
-    
-
-    return (
-        <>
-            <LikesIcon onClick={clickHandler}  /> Likes: {likes ? (likes) : (0)}
-        </>
-    )
-}
-
-export default Likes
-
+export default Likes;
--- client/src/components/pages/articles/responses/NewPost.js
+++ client/src/components/pages/articles/responses/NewPost.js
@@ -1,142 +1,133 @@
-import React, {useState, useContext, useRef, useEffect} from 'react'
-import { styled } from '@mui/material/styles';
-import styles from 'styled-components';
-import { ValidateCommentType, ValidatePost } from './postValidator'
-import {Button, Box} from '@mui/material'
-import axios from 'axios';
-import Config from '../../../../config/index'
-import { UserContext } from '../../../../App';
-import { ArticleInfoContext} from '../ViewArticle'
-import Gravatar from 'react-gravatar';
-import SendIcon from '@mui/icons-material/Send';
-
+import React, { useState, useContext, useRef, useEffect } from "react";
+import { styled } from "@mui/material/styles";
+import styles from "styled-components";
+import { ValidateCommentType, ValidatePost } from "./postValidator";
+import { Button, Box } from "@mui/material";
+import axios from "axios";
+import Config from "../../../../config/index";
+import { UserContext } from "../../../../App";
+import { ArticleInfoContext } from "../ViewArticle";
+import Gravatar from "react-gravatar";
+import SendIcon from "@mui/icons-material/Send";
 
 const NewPost = (props) => {
-    const [error, setError] = useState('');
-    const [user, setUser] = useContext(UserContext);
-    const [articleInfo, setArticleInfo] = useContext(ArticleInfoContext);
-    const postRef = useRef();
-    const [clickedValueForCommentAndIssue, setClickedValueForCommentAndIssue] = useState('');
+  const [error, setError] = useState("");
+  const [user, setUser] = useContext(UserContext);
+  const [articleInfo, setArticleInfo] = useContext(ArticleInfoContext);
+  const postRef = useRef();
+  const [clickedValueForCommentAndIssue, setClickedValueForCommentAndIssue] =
+    useState("");
 
-    const [newPost, setNewPost ] = useState(
-        {
-            userResponse_type: '',
-            post_content: '',
-            article_id: "",
-            user_id: ""
-        }
-    )
-    
-   useEffect(()=>{
-     if(newPost.post_content){
-     postRef.current.value = newPost.post_content;
-     postRef.current.focus();
-     }
-   })
-    
-    const handleNewPost = event => {
-        
-        if(props.article_id == '') {
-            //console.log("failed to load article id")
-            setError('failed to load article id.')
-        } else if(ValidatePost(newPost.post_content)) {
-            //console.log('test for text')
-            setError('Please enter a post.')
-            //console.log(error)
-        } else if(ValidateCommentType(newPost.userResponse_type)) {
-            //console.log(newPost.userResponse_type)
-            setError('Please select comment or issue')
-            //console.log(error)
-        } else {
-            //console.log("texting else")          
-            if(newPost.article_id != "") {
-                axios.post(`${Config.URL}api/deleteLike`, {
-                    post: newPost
-                })
-                window.location.reload();
-            }
+  const [newPost, setNewPost] = useState({
+    userResponse_type: "",
+    post_content: "",
+    article_id: "",
+    user_id: "",
+  });
 
-
-        }
-
+  useEffect(() => {
+    if (newPost.post_content) {
+      postRef.current.value = newPost.post_content;
+      postRef.current.focus();
     }
+  });
 
-    const handlePostChange = (event) =>{
-        setNewPost({...newPost, post_content: event.target.value})
+  const handleNewPost = (event) => {
+    if (props.article_id == "") {
+      //console.log("failed to load article id")
+      setError("failed to load article id.");
+    } else if (ValidatePost(newPost.post_content)) {
+      //console.log('test for text')
+      setError("Please enter a post.");
+      //console.log(error)
+    } else if (ValidateCommentType(newPost.userResponse_type)) {
+      //console.log(newPost.userResponse_type)
+      setError("Please select comment or issue");
+      //console.log(error)
+    } else {
+      //console.log("texting else")
+      if (newPost.article_id != "") {
+        console.log(newPost);
+        axios.post(`${Config.URL}api/creatPost`, {
+          post: newPost,
+        });
+        window.location.reload();
+      }
     }
+  };
 
-    const FormattedGroup = styled("div") ({
-        display: 'flex',
-        justifyContent: 'flex-end'
-    })
-    const FormattedRadio = styled(Button)((props)=> ({
-        backgroundColor: props.isclicked.color,
-        boxShadow: props.isclicked.shadow,
-        color: '#FFFFFF',
-        width: '150px',
-        textAlign: 'center',
-        marginLeft: '10px',
-        marginBottom: '5px',
-        fontSize: '1.2em',
-        padding: '5px 5px',
-        borderRadius: '5px',
-        fontFamily:'Acme, sans-serif',
-        '&:hover': {
-            backgroundColor: '#213946'
-        },
-        '&:focus': {
-            backgroundColor: '#213946'
-        },
-        
-    }))
+  const handlePostChange = (event) => {
+    setNewPost({ ...newPost, post_content: event.target.value });
+  };
 
-    const LeaveCommentGroup = styled('div')({
-        position: 'relative',
-        '.profileImage':{
-            boxShadow:'3px 3px 20px black',
-            outline: '5px double #033F63',
-            marginRight:'10px',
-            height:'60px', 
-            width:'60px',
-            position:'absolute',
-            top:'18px',
-            left:'16px',
-            ['@media (max-width:500px)']: { 
-                height:'45px', 
-                width:'45px',
-                top: '26px',
-             }
-        },
-        
-    })
+  const FormattedGroup = styled("div")({
+    display: "flex",
+    justifyContent: "flex-end",
+  });
+  const FormattedRadio = styled(Button)((props) => ({
+    backgroundColor: props.isclicked.color,
+    boxShadow: props.isclicked.shadow,
+    color: "#FFFFFF",
+    width: "150px",
+    textAlign: "center",
+    marginLeft: "10px",
+    marginBottom: "5px",
+    fontSize: "1.2em",
+    padding: "5px 5px",
+    borderRadius: "5px",
+    fontFamily: "Acme, sans-serif",
+    "&:hover": {
+      backgroundColor: "#213946",
+    },
+    "&:focus": {
+      backgroundColor: "#213946",
+    },
+  }));
 
-    const FormattedButton = styled(Button)({
-        background: '#033F63',
-        color: '#FFFFFF',
-        maxWidth: '150px',
-        padding: '10px',
-        '&:hover': {
-            backgroundColor: '#213946',
-            transform: 'scale(1.2)'
-        },
-        position:'absolute',
-        right: '10px',
-        top: '31px',
-        transition: '0.3s ease all',
-        fontFamily:'Acme, sans-serif',
-        boxShadow:'1px 1px 5px black',
-        ['@media (max-width:500px)']: { 
-            '.icon':{
-                fontSize:'1.5em',
-                marginLeft:'2px'
-            },
-            minWidth:'40px',
-            right:'22px'
-            
-         }
-        
-    })
-    const StyledTextArea = styles.textarea`
+  const LeaveCommentGroup = styled("div")({
+    position: "relative",
+    ".profileImage": {
+      boxShadow: "3px 3px 20px black",
+      outline: "5px double #033F63",
+      marginRight: "10px",
+      height: "60px",
+      width: "60px",
+      position: "absolute",
+      top: "18px",
+      left: "16px",
+      ["@media (max-width:500px)"]: {
+        height: "45px",
+        width: "45px",
+        top: "26px",
+      },
+    },
+  });
+
+  const FormattedButton = styled(Button)({
+    background: "#033F63",
+    color: "#FFFFFF",
+    maxWidth: "150px",
+    padding: "10px",
+    "&:hover": {
+      backgroundColor: "#213946",
+      transform: "scale(1.2)",
+    },
+    position: "absolute",
+    right: "10px",
+    top: "31px",
+    transition: "0.3s ease all",
+    fontFamily: "Acme, sans-serif",
+    boxShadow: "1px 1px 5px black",
+    ["@media (max-width:500px)"]: {
+      ".icon": {
+        fontSize: "1.5em",
+        marginLeft: "2px",
+      },
+      minWidth: "40px",
+      right: "22px",
+    },
+  });
+  const StyledTextArea = styles.textarea`
         width: 100%;
         min-height: 100px;
         resize: vertical;
@@ -151,56 +142,80 @@ const NewPost = (props) => {
         @media (max-width:500px){ 
             padding: 10px 75px;
          }
-    `
+    `;
 
-    const ErrorMessage = styled(Box) ({
-        color: "red",
-        textSize: '1.7em'
-    })
-    
-    const handleToggle = (event) => {
-        setNewPost({...newPost, userResponse_type: event.target.value,  user_id: articleInfo.user_id, article_id: articleInfo.article_id});
-        setClickedValueForCommentAndIssue(event.target.value);
-        //console.log('set type', event.target.value)
-    }
+  const ErrorMessage = styled(Box)({
+    color: "red",
+    textSize: "1.7em",
+  });
 
-    //console.log("Testing new post ", newPost)
+  const handleToggle = (event) => {
+    setNewPost({
+      ...newPost,
+      userResponse_type: event.target.value,
+      user_id: articleInfo.user_id,
+      article_id: articleInfo.article_id,
+    });
+    setClickedValueForCommentAndIssue(event.target.value);
+    //console.log('set type', event.target.value)
+  };
 
-    return (
-        <>
-        <FormattedGroup aria-label="type" >
-            <FormattedRadio value="comment" onClick={handleToggle} name="Comment" isclicked={'comment' === clickedValueForCommentAndIssue ? {color:'black', shadow:'1px 1px 10px black'}:{color:'#033F63', shadow:'none'}}>Comment</FormattedRadio>
-            <FormattedRadio value="issue" onClick={handleToggle} name="Issue" isclicked={'issue'=== clickedValueForCommentAndIssue ? {color:'black', shadow:'1px 1px 10px black '}:{color:'#033F63', shadow:'none'}} >Issue</FormattedRadio>
-        </FormattedGroup>
-            <LeaveCommentGroup>
-            <Gravatar
-                className='profileImage'
-	            email={'xxxx@gmail.com'}
-	            size={100}
-	            rating="pg"
-	            default="mp"
-	            style={{margin: '0 10px 0 0', borderRadius:'50%', display:'block'}}
-            />
-            <StyledTextArea 
-                ref={postRef}
-                name="post_content" 
-                onChange={handlePostChange}
-            />
+  //console.log("Testing new post ", newPost)
 
-            <FormattedButton onClick={handleNewPost}
-            ><SendIcon className="icon"/>
-            </FormattedButton>
-            </LeaveCommentGroup>
-            {error != '' ? 
-            (
-                <ErrorMessage>
-                {error}
-                </ErrorMessage>
-            ) 
-                : (console.log(error))
-            }
-        </>
-    )
-}
+  return (
+    <>
+      <FormattedGroup aria-label="type">
+        <FormattedRadio
+          value="comment"
+          onClick={handleToggle}
+          name="Comment"
+          isclicked={
+            "comment" === clickedValueForCommentAndIssue
+              ? { color: "black", shadow: "1px 1px 10px black" }
+              : { color: "#033F63", shadow: "none" }
+          }
+        >
+          Comment
+        </FormattedRadio>
+        <FormattedRadio
+          value="issue"
+          onClick={handleToggle}
+          name="Issue"
+          isclicked={
+            "issue" === clickedValueForCommentAndIssue
+              ? { color: "black", shadow: "1px 1px 10px black " }
+              : { color: "#033F63", shadow: "none" }
+          }
+        >
+          Issue
+        </FormattedRadio>
+      </FormattedGroup>
+      <LeaveCommentGroup>
+        <Gravatar
+          className="profileImage"
+          email={"xxxx@gmail.com"}
+          size={100}
+          rating="pg"
+          default="mp"
+          style={{
+            margin: "0 10px 0 0",
+            borderRadius: "50%",
+            display: "block",
+          }}
+        />
+        <StyledTextArea
+          ref={postRef}
+          name="post_content"
+          onChange={handlePostChange}
+        />
 
-export default NewPost
+        <FormattedButton onClick={handleNewPost}>
+          <SendIcon className="icon" />
+        </FormattedButton>
+      </LeaveCommentGroup>
+      {error != "" ? <ErrorMessage>{error}</ErrorMessage> : console.log(error)}
+    </>
+  );
+};
+
+export default NewPost;
