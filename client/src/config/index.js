a5beae2a47e15ededc0337c64a3ae9b9e075b588
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2021-12-05 09:57:48 -0500

    updated build

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,6 +1,6 @@
 const Config = {
-  URL: "http://localhost:5000/",
-  //URL : "https://knowledge-base-article-system.vercel.app/"
+  //URL: "http://localhost:5000/",
+  URL : "https://knowledge-base-article-system.vercel.app/"
 };
 
 export default Config;
